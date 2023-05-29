93afc4e35a9dc77fed3867470c94f9f30ad94a3d
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-30 01:47:31 +0300

    Fix linte errrs

--- src/modules/header-footer.js
+++ src/modules/header-footer.js
@@ -1,20 +1,15 @@
 import logo from './assets/lib-logo.png';
 
 const header = `
-
 <img class="logo" src="${logo}" alt="Library logo">
 <h3 id="count-books">Books(6)</h3>
 <h3>Fiction</h3>
 <h3>Educative</h3> 
-
 `;
 
 const footer = `
-
 <p>Created by Microverse students: Sebastian and Sylvester under CC license</p>
-
 <div class="footer-grey" >
-
 </div>
 `;
 
