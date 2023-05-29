b62e12fe85403e98355b325855f9877bd61650d8
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-29 23:03:26 +0300

    Add header code in header module

--- src/modules/header.js
+++ src/modules/header.js
@@ -1,3 +1,10 @@
 const header = `
 
+<img src="src/asset/lib logo.png" alt="Library logo">
+<div><h2>Books</h2><span>(6)</span></div>
+<h2>Fiction</h2>
+<h2>Educative</h2> 
+
 `
+
+export default header
