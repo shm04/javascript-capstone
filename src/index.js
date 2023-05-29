a80500bb63aff2de17d8ed185b6115baad400793
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-30 01:28:49 +0300

    Apped footer section to the DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './styles/main.css';
-import header from './modules/header.js';
+import {header, footer} from './modules/header-footer.js';
 
 const headerSection = document.createElement('header');
+const footerSection = document.createElement('footer');
 headerSection.innerHTML = header;
+footerSection.innerHTML = footer;
 document.body.appendChild(headerSection);
+document.body.appendChild(footerSection);
