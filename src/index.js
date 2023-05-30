8f8a604ff67a769c00bebc6534b9cd286f1cc4db
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-30 17:48:39 +0300

    Move the header and footer code to html file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 import './styles/main.css';
-import { header, footer } from './modules/header-footer.js';
-
-const headerSection = document.createElement('header');
-const footerSection = document.createElement('footer');
-headerSection.innerHTML = header;
-footerSection.innerHTML = footer;
-document.body.appendChild(headerSection);
-document.body.appendChild(footerSection);
