2b57be2f7fcfea2b4bb3fb899a4e29643366433a
Sylvester Wamaya <sylvesterwamaya@gmail.com>
2023-05-31 22:51:32 +0300

    Import comment module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './styles/main.css';
-
+import commentPop from './modules/comment.js';
 import displayBook from './modules/displayBooks.js';
 
 window.addEventListener('load', () => {
   displayBook();
 });
+commentPop()
